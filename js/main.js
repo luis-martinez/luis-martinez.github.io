af523a36242b46c2c4df751568b9a307c7141e14
Luis Martinez <luis-martinez@users.noreply.github.com>
2016-05-07 16:22:12 -0700

    Added contact form

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+$('#contact').validate({
+submitHandler: function(form) {
+    $(form).ajaxSubmit({
+    url: 'process.php',
+    success: function() {
+    $('#contact').hide();
+    $('#contact-form').append("<p class='thanks'>Thanks! Your request has been sent.</p>")
+    }
+    });
+    }
+});
+});
